11996fb30fbbd6898753324e0e4a66adf836fccb
Slaimon <simo.bertolucci@gmail.com>
2025-09-10 03:13:39 +0200

    Minor refactor of port code

--- port/random.js
+++ port/random.js
@@ -1,131 +1,137 @@
 export {
-    bits,
-    int,
-    float,
-    full_init,
-    init
+   bits,
+   int,
+   float,
+   full_init,
+   init
 }
 
+const defaultSeed =
+      [ 509760043,
+         399328820,
+         99941072,
+         112282318,
+         611886020,
+         516451399,
+         626288598,
+         337482183,
+         748548471,
+         808894867,
+         657927153,
+         386437385,
+         42355480,
+         977713532,
+         311548488,
+         13857891,
+         307938721,
+         93724463,
+         1041159001,
+         444711218,
+         1040610926,
+         233671814,
+         664494626,
+         1071756703,
+         188709089,
+         420289414,
+         969883075,
+         513442196,
+         275039308,
+         918830973,
+         598627151,
+         134083417,
+         823987070,
+         619204222,
+         81893604,
+         871834315,
+         398384680,
+         475117924,
+         520153386,
+         324637501,
+         38588599,
+         435158812,
+         168033706,
+         585877294,
+         328347186,
+         293179100,
+         671391820,
+         846150845,
+         283985689,
+         502873302,
+         718642511,
+         938465128,
+         962756406,
+         107944131,
+         192910970 ];
 
-function defaultSeed() {
-return [
-   509760043,
-   399328820,
-   99941072,
-   112282318,
-   611886020,
-   516451399,
-   626288598,
-   337482183,
-   748548471,
-   808894867,
-   657927153,
-   386437385,
-   42355480,
-   977713532,
-   311548488,
-   13857891,
-   307938721,
-   93724463,
-   1041159001,
-   444711218,
-   1040610926,
-   233671814,
-   664494626,
-   1071756703,
-   188709089,
-   420289414,
-   969883075,
-   513442196,
-   275039308,
-   918830973,
-   598627151,
-   134083417,
-   823987070,
-   619204222,
-   81893604,
-   871834315,
-   398384680,
-   475117924,
-   520153386,
-   324637501,
-   38588599,
-   435158812,
-   168033706,
-   585877294,
-   328347186,
-   293179100,
-   671391820,
-   846150845,
-   283985689,
-   502873302,
-   718642511,
-   938465128,
-   962756406,
-   107944131,
-   192910970];
-}
+const defaultState = [ defaultSeed, 0 ];
 
-var defaultState = [defaultSeed(), 0];
-
-// restituisce un array di lunghezza k
-// se k<s.length, allora taglia s
+// restituisce un array di lunghezza k.
+// se k < s.length, allora taglia s
 // altrimenti restituisce s+'aaaaa...'
 function pad(k, s) {
-var n = s.length;
-if (n <= k)
-   return s + 'a'.repeat(k - n);
-else
-   return s.slice(n - k);
+   var n = s.length;
+   if (n <= k)
+      return s + 'a'.repeat(k - n);
+   else
+      return s.slice(n - k);
 };
 
 function state_full_init(s, seed) {
-function combine(accu, x) {
-     return pad(16, accu + x.toString() );
-};
-function extract(d) {
-    return d.charCodeAt(0) + (d.charCodeAt(1) << 8) + (d.charCodeAt(2) << 16) ^ d.charCodeAt(3) << 22;
-  }
-var l = seed.length;
-for (var i = 0; i <= 54; i++) s[0][i] = i;
-var accu = "x";
-for (var i = 0; i <= 54 + Math.max(55, l); i++) {
-   var j = i % 55;
-   var k = i % l;
-   accu = combine(accu, seed[k]);
-   s[0][j] = s[0][j] ^ extract(accu);
-}
-return s[1] = 0;
+   function combine(accu, x) {
+      return pad(16, accu + x.toString());
+   };
+   function extract(d) {
+      return d.charCodeAt(0) + (d.charCodeAt(1) << 8)
+            + (d.charCodeAt(2) << 16) ^ d.charCodeAt(3) << 22;
+   }
+   var l = seed.length;
+   for (var i = 0; i <= 54; i++) s[0][i] = i;
+   var accu = "x";
+   for (var i = 0; i <= 54 + Math.max(55, l); i++) {
+      var j = i % 55;
+      var k = i % l;
+      accu = combine(accu, seed[k]);
+      s[0][j] = s[0][j] ^ extract(accu);
+   }
+   return s[1] = 0;
 };
 
 function state_bits(s) {
-s[1] = (s[1] + 1) % 55;
-var newval = s[0][(s[1] + 24) % 55] + s[0][s[1]] & 1073741823;
-s[0][s[1]] = newval;
-return newval;
+   s[1] = (s[1] + 1) % 55;
+   var newval = s[0][(s[1] + 24) % 55] + s[0][s[1]] & 1073741823;
+   s[0][s[1]] = newval;
+   return newval;
 };
+
 function state_intaux(s, n) {
-var r = state_bits(s);
-var v = r % n;
-if (r - v > 1073741823 - n + 1) return state_intaux(s, n);
-return v;
+   var r = state_bits(s);
+   var v = r % n;
+   if (r - v > 1073741823 - n + 1)
+      return state_intaux(s, n);
+   return v;
 };
+
 function state_int(s, bound) {
-if (bound > 1073741823 || bound <= 0) { throw new Error("random.state_int: invalid input " + bound); }
-return state_intaux(s, bound);
+   if (bound > 1073741823 || bound <= 0)
+      throw new Error("random.state_int: invalid input " + bound);
+   return state_intaux(s, bound);
 };
+
 function rawfloat(s) {
-var scale = 1073741824.0;
-var r0 = state_bits(s);
-var r1 = state_bits(s);
-var r2 = state_bits(s);
-return ((r0 / scale + r1) / scale + r2) / scale;
+   var scale = 1073741824.0;
+   var r0 = state_bits(s);
+   var r1 = state_bits(s);
+   var r2 = state_bits(s);
+   return ((r0 / scale + r1) / scale + r2) / scale;
 };
-function state_float(s, bound) { return rawfloat(s) * bound; };
+
+function state_float(s, bound) {
+   return rawfloat(s) * bound;
+};
 
 //*    EXPORTS     *//
 
-function bits(param) { return state_bits(defaultState); };
+function bits(unit) { return state_bits(defaultState); };
 function int(bound) { return state_int(defaultState, bound); };
 function float(scale) { return state_float(defaultState, scale); };
 function full_init(seed) { return state_full_init(defaultState, seed); };
--- port/util.js
+++ port/util.js
@@ -2,57 +2,57 @@ import * as random from "./random.js"
 import * as list   from "./list.js"
 
 export {
-         mod_float,
-         range,
-         rgb_range,
-         rnd_int,
-         rnd_float,
-         split_name,
-         nest,
-         rnd_partition,
-         pick_exp,
-         pick,
-         pick_many,
-         unionq,
-         rgb_of_color,
-         rgb_of_hsl,
-         rnd_color,
-         prng_init,
+   mod_float,
+   range,
+   rgb_range,
+   rnd_int,
+   rnd_float,
+   split_name,
+   nest,
+   rnd_partition,
+   pick_exp,
+   pick,
+   pick_many,
+   unionq,
+   rgb_of_color,
+   rgb_of_hsl,
+   rnd_color,
+   prng_init,
 }
 
 // a%b ma generalizzato ai float
 function mod_float(a, b) {
-     var x = a / b;
-     var n = Math.floor(Math.abs(x));
-     if (x >= 0.0) return a - n * b;
-     return a + n * b;
-   }
+   var x = a / b;
+   var n = Math.floor(Math.abs(x));
+   if (x >= 0.0) return a - n * b;
+   return a + n * b;
+}
 
 // f(f(...f(f(x))...)), n volte f
 function nest(f, x, n) {
-     if (n <= 0) return x;
-     return f(nest(f, x, n - 1));
+   if (n <= 0) return x;
+   return f(nest(f, x, n - 1));
 }
 
 
 
-                  /////////////////////////////////////////////////
-                  ///**    FUNZIONI DI GENERAZIONE CASUALE    **///
+/////////////////////////////////////////////////
+///**    FUNZIONI DI GENERAZIONE CASUALE    **///
 
 
 // inizializza il prng con una stringa!
 function prng_init(str) {
-  // se str != stringa vuota
-  if (str !== '') {
-    var n = str.length;
-    var a = Array(n).fill(0);
-    for (var i = 0; i <= n - 1; i++) {
+   // se str != stringa vuota
+   if (str !== '') {
+      var n = str.length;
+      var a = Array(n).fill(0);
+      for (var i = 0; i <= n - 1; i++) {
       a[i] = i*i + str.charCodeAt(i);
-    }
-    return random.full_init(a);
-  }
-  // default seed
-  return random.full_init([0]);
+      }
+      return random.full_init(a);
+   }
+   // default seed
+   return random.full_init([0]);
 }
 
 // random number in interval
@@ -63,53 +63,54 @@ function rnd_float(u, v) { return u + random.float(v - u); }
 // il sottoinsieme è rappresentato da una lista ocaml di k elementi
 // di cui q sono pari a 1 e tutti gli altri 0 
 function rnd_partition(q, k) {
-  function inc(lst) {
-     var n = lst[0];
-     var flag = false;
-     start: {
-       if (n) { flag = true; break start; }
-       var match1 = lst[1];
-       if (!match1) { flag = true; break start; }
-       return [match1[0] + 1, match1[1]];
-     }
-     if (flag) {
-       {
+   function inc(lst) {
+      var n = lst[0];
+      var flag = false;
+      start: {
+         if (n) { flag = true; break start; }
+         var match1 = lst[1];
+         if (!match1) { flag = true; break start; }
+         return [match1[0] + 1, match1[1]];
+      }
+      if (flag) {
          var match2 = lst[1];
-         if (match2) return [match2[0], inc([n - 1, match2[1]])];
+         if (match2)
+            return [match2[0], inc([n - 1, match2[1]])];
          throw new Error("util.rnd_partition: an impossible thing happened");
-       }
-     }
-  }
+      }
+   }
 
-  return nest(function (is) {
-        var r = rnd_int(0, k - 1);
-        return inc([r, is]);
-      }, nest(function (zs) { return [0, zs]; }, 0, k), q);
+   return nest(function (is) {
+                  var r = rnd_int(0, k - 1);
+                  return inc([r, is]);
+               },
+               nest(function (zs) { return [0, zs]; }, 0, k),
+               q);
 }
 
 
 
 
-                  ////////////////////////////////////////////
-                  ///**    FUNZIONI DI SCELTA CASUALE    **///
+////////////////////////////////////////////
+///**    FUNZIONI DI SCELTA CASUALE    **///
 
 
 // random pick function with exponential distribution ??
 function pick_exp(p, lst) {
-  function pck(u, v, l) {
-    if (l) {
-       var xs = l[1];
-       var x = l[0];
-       if (!xs) return x;
-       if (v <= u) return x;
-       return pck(u * (1.0 - p), v - u, xs);
-    }
-    throw new Error("util.pick_exp: cannot pick from empty list");
-  }
+   function pck(u, v, l) {
+      if (!l)
+         throw new Error("util.pick_exp: cannot pick from empty list");
 
-  var n = list.len(lst);
-  var q = rnd_float(0.0, 1.0 - Math.pow(1.0 - p, n));
-  return pck(p, q, lst);
+      var xs = l[1];
+      var x = l[0];
+      if (!xs) return x;
+      if (v <= u) return x;
+      return pck(u * (1.0 - p), v - u, xs);
+   }
+
+   var n = list.len(lst);
+   var q = rnd_float(0.0, 1.0 - Math.pow(1.0 - p, n));
+   return pck(p, q, lst);
 }
 
 // lst[rnd_int]
@@ -158,32 +159,30 @@ function pick_many(n, lst) {
 // (il primo membro della lista non contiene mai spazio, 
 // A MENO CHE la stringa originale non ne contenesse nessuno. probabile bug)
 function split_name(str) {
-  var k = str.indexOf(' ');
-  if (k != -1) {
-      return [
-          str.slice(0, k),
-          str.slice(k + 1)
-      ];
-  } else {
-      return [str+' ', str];
-  }
+   var k = str.indexOf(' ');
+   if (k != -1) {
+      return [ str.slice(0, k),
+               str.slice(k + 1) ];
+   } else {
+      return [ str + ' ', str ];
+   }
 };
 
 // appends x to the end of the list iff no element in lst is === x
 function unionq(x, lst) {
-  if (lst) {
-    var y = lst[0];
-    if (x === y) return lst;
-    return [y, unionq(x, lst[1])];
-  }
-  return [x, 0];
+   if (lst) {
+      var y = lst[0];
+      if (x === y) return lst;
+      return [y, unionq(x, lst[1])];
+   }
+   return [x, 0];
 }
 
 
 
 
-            /////////////////////////////////////
-            ///**    FUNZIONI SUI COLORI    **///
+/////////////////////////////////////
+///**    FUNZIONI SUI COLORI    **///
 
 
 // ???
@@ -194,7 +193,7 @@ function range(a, b, min, max, x) {
    }
    return max -
             (max - min) *
-              Math.abs(mod_float(Math.abs(x - a), 2.0 * (b - a)) / (b - a) - 1.0);
+               Math.abs(mod_float(Math.abs(x - a), 2.0 * (b - a)) / (b - a) - 1.0);
  }
 
 // converte x da (-inf,+inf) a un intervallo scelto (default [-1,1])
@@ -203,16 +202,14 @@ function rgb_range(opt_min, opt_max, x) {
    var min = opt_min ? opt_min[0] : -1.0;
    var max = opt_max ? opt_max[0] : 1.0;
    return min + (max - min) * (0.5 + Math.atan(2.0 * x) / Math.PI);
- }
+}
 
 // converte un vec3 da (-inf, +inf) a [0,255]
 function rgb_of_color(color) {
-   return [
-     rgb_range([0.0], [255.0], color[0]) >> 0,    // shifting by 0 truncates to 32-bit integer
-     rgb_range([0.0], [255.0], color[1]) >> 0,
-     rgb_range([0.0], [255.0], color[2]) >> 0
-   ];
- }
+   return [ rgb_range([0.0], [255.0], color[0]) >> 0,    // shifting by 0 truncates to 32-bit integer
+            rgb_range([0.0], [255.0], color[1]) >> 0,
+            rgb_range([0.0], [255.0], color[2]) >> 0 ];
+}
 
 // anche questa si spiega da sé, ma andrebbe testata per scoprire
 // se ci sono quirk di implementazione da replicare
@@ -221,7 +218,9 @@ function rgb_of_color(color) {
 // per cercare di isolare eventuali problemi
 function rgb_of_hsl(h, sl, l) {
    var v = l <= 0.5 ? l * (1.0 + sl) : l + sl - l * sl;
-   if (v <= 0.0) return [0.0, 0.0, 0.0];
+   if (v <= 0.0)
+      return [0.0, 0.0, 0.0];
+
    var m = l + l - v;
    var sv = (v - m) / v;
    var h6 = Math.abs(h * 6.0);
@@ -230,17 +229,19 @@ function rgb_of_hsl(h, sl, l) {
    var vsf = v * sv * fract;
    var mid1 = m + vsf;
    var mid2 = v - vsf;
-   if (sextant < 0 || sextant > 4) return [v, m, mid2];
-   switch (sextant)
-   {
-   case 0: return [v, mid1, m];
-   case 1: return [mid2, v, m];
-   case 2: return [m, v, mid1];
-   case 3: return [m, mid2, v];
-   case 4: return [mid1, m, v];
-   default: return null;
+
+   if (sextant < 0 || sextant > 4)
+      return [v, m, mid2];
+   
+   switch (sextant) {
+      case 0: return [v, mid1, m];
+      case 1: return [mid2, v, m];
+      case 2: return [m, v, mid1];
+      case 3: return [m, mid2, v];
+      case 4: return [mid1, m, v];
+      default: return null;
    }
- }
+}
 
 // random color in the rgb [0,255] range
 function rnd_color() {
@@ -248,4 +249,4 @@ function rnd_color() {
    var g = rnd_float(-1.0, 1.0);
    var b = rnd_float(-1.0, 1.0);
    return [r, g, b];
- }
+}
